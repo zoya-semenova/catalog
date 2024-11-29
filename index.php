8412ac680632bb33172ba14fd260db2f91ea0332
Zoya <zoya.semenova@mail.ru>
2024-11-30 01:46:57 +0500

    fix

--- index.php
+++ index.php
@@ -2,12 +2,11 @@
 ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
-function buildCategoryTree(array $elements, $parentId = 0) {
+function buildCategoryTree(array $elements, $parentId = 0)
+{
     $branch = [];
 
-//echo "<pre>";print_r($elements);print_r($parentId);
     foreach ($elements as $element) {
-        //$element['level'] = $depth;
         if ($element['id_parent'] == $parentId) {
             $children = buildCategoryTree($elements, $element['id']);
             if ($children) {
@@ -22,12 +21,12 @@ function buildCategoryTree(array $elements, $parentId = 0) {
 
 function buildTreeIds(array $elements, $parentId = 0)
 {
-    $ids = [];
-//echo "<pre>";print_r($elements);print_r($parentId);
+    $ids = [$parentId];
+
     foreach ($elements as $element) {
-        //$element['level'] = $depth;
-        if ($element['id_parent'] == $parentId ) {
-            $ids = buildTreeIds($elements, $element['id']);
+        if ($element['id_parent'] == $parentId) {
+            $children = buildTreeIds($elements, $element['id']);
+            $ids = array_merge($ids, $children);
             $ids[] = $element['id'];
         }
     }
@@ -35,12 +34,14 @@ function buildTreeIds(array $elements, $parentId = 0)
     return $ids;
 }
 
-function renderCategoryTree($categoryTree, $html = "") {
-    $html .= '<ul>';
+function renderCategoryTree($categoryTree, $parentId, $html = "")
+{
+    $html .= '<ul style="display:' . ($parentId ? "none" : "block") . '">';
     foreach ($categoryTree as $category) {
-        $html .= "<li><a href = '/?group=". $category['id'] . "'>". $category['name']."</a>";
+        $html .= "<li>
+<a  id=" . $category['id'] . " href = '/?group=" . $category['id'] . "'>" . $category['name'] . "</a>";
         if (!empty($category['children'])) {
-            $html .= renderCategoryTree($category['children']);
+            $html .= renderCategoryTree($category['children'], $category['id']);
         }
         $html .= '</li>';
     }
@@ -49,13 +50,13 @@ function renderCategoryTree($categoryTree, $html = "") {
     return $html;
 }
 
-function renderProducts($categoryTree) {
-    $html = '<ul>';
+function renderProducts($categoryTree)
+{
+    $html = "";
     foreach ($categoryTree as $category) {
         $html .= '<li>' . $category['name'];
         $html .= '</li>';
     }
-    $html .= '</ul>';
 
     return $html;
 }
@@ -63,7 +64,7 @@ function renderProducts($categoryTree) {
 try {
     $pdo = new PDO("mysql:host=localhost;dbname=catalog", "root", "root");
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch(PDOException $e) {
+} catch (PDOException $e) {
     die("Connection failed: " . $e->getMessage());
 }
 
@@ -75,108 +76,77 @@ if (!empty($_GET['group'])) {
 $sql = $pdo->prepare("SELECT * FROM `groups`");
 $sql->execute();
 $rows = $sql->fetchAll();
-//echo "<pre>";print_r($rows);
 
 $categoryTree = buildCategoryTree($rows);
-//echo "<pre>";print_r($categoryTree);exit;
 
-    $categoryTreeRender = renderCategoryTree($categoryTree);
+$categoryTreeRender = renderCategoryTree($categoryTree, $categoryId);
 
-    $ids = buildTreeIds($rows, $categoryId);
-    $sql = $pdo->prepare("SELECT * FROM `products` WHERE `id_group` IN (".implode(',',$ids).")");
-    $sql->execute();
-    $rows = $sql->fetchAll();
-    $products = renderProducts($rows);
+$ids = buildTreeIds($rows, $categoryId);
 
+$where = "";
+if (!empty($ids)) {
+    $where = "WHERE `id_group` IN (" . implode(",", $ids) . ")";
+}
+$sql = $pdo->prepare("SELECT * FROM `products` " . $where);
+$sql->execute();
+$rows = $sql->fetchAll();
+$products = renderProducts($rows);
 
-// $categoryTree from step 2
-
-//echo "<pre>";print_r(implode(',', array_column($categoryTree, 'id')));exit;
-
-
-    //echo "<pre>";print_r($rows);exit;
-
-
-
-//echo "<pre>";print_r($categoryTree);exit;
+if ($categoryId) {
+    echo $products;
+    exit;
+}
 ?>
-<table>
-    <tr>
-        <th>Categories</th>
-        <th>Products</th>
-    </tr>
-    <tr>
-        <td><?= $categoryTreeRender?></td>
-        <td><?= $products?></td>
-    </tr>
-</table>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
 <table>
     <tr>
-        <th>Categories</th>
-        <th>Products</th>
     </tr>
     <tr>
         <td>
-            <a id='category-a' style='text-decoration:none; color:black;' href='#'>
-                <span  id='category-symbol'>+ </span>
+            <a id='category' style='text-decoration:none; color:black;' href='#'>
+                <span id='category-symbol'>+ </span>
                 Category
             </a>
-            <ul class='child'  id='category'>
-                <?= $categoryTreeRender ?>
-            </ul>
+            <?= $categoryTreeRender ?>
         </td>
         <td>
-            <a id='category2-a' style='text-decoration:none; color:black;' href='#'>
-                <span  id='category2-symbol'>+ </span>
+            <a id='products' style='text-decoration:none; color:black;' href='#'>
+                <span id='products-symbol'>+ </span>
                 Products
             </a>
-            <ul class='child'  id='category2'>
+            <ul>
                 <?= $products ?>
             </ul>
         </td>
-
     </tr>
 </table>
 
 <script>
-    $(document).ready(function(){
-        $('.child').hide();
+    $(document).ready(function () {
+        $('.child').next('ul').hide();
+
+        $("body").on('click', "a", function (e) {
+          //  if ($(this).next().find('ul').is(":visible")) {
+            href = $(this);
+            //console.log(href.attr('href'));
+            $.ajax({
+                url: typeof href.attr('href') === 'undefined' ? 0 : href.attr('href'),
+                method: 'get',
+                dataType: 'html',
+                success: function (data) {
+                    if (href.next('ul').is(":visible")) {
+                        href.next('ul').hide()
+                    } else {
+                        href.next('ul').show();
+                    }
+
+                    $('#products').next('ul').html(data);
+                }
+            });
+
+            e.stopPropagation();
+            e.preventDefault();
+        });
     });
-    $( "a" ).click(function() {
-        Show(this);
-    });
-
-    function Show(obj)
-    {
-        var ObjId = obj.id;
-        var Obj = ObjId.split('-');
-        var id = Obj[0];
-
-        var symb = $('#'+id+'-symbol').html();
-
-        if(symb.trim() == "+")
-        {
-            $('#'+id).show(1000);
-            $('#'+id+'-symbol').html("- ");
-        }
-        else
-        {
-            $('#'+id).hide(1000);
-            $('#'+id+'-symbol').html("+ ");
-        }
-
-    }
-/*
-    $.ajax({
-        url: '/index.php',
-        method: 'get',
-        dataType: 'html',
-        data: {group: '2'},
-        success: function(data){
-            alert(data);
-        }
-    });
-*/
 </script>
